4704436ef10359b291ddd0d818ea25de9599e112
KSH701 <rkrk7636@naver.com>
2026-01-18 22:57:53 +0900

    01/18 문제풀이

---
+++ level1/033-이상한-문자열-만들기.js
@@ -0,0 +1,15 @@
+function solution(s) {
+  const arr = [];
+  let idx = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === " ") {
+      arr.push(" ");
+      idx = 0;
+    } else {
+      arr.push(idx % 2 === 0 ? s[i].toUpperCase() : s[i].toLowerCase());
+      idx++;
+    }
+  }
+  return arr.join("");
+}
+console.log(solution("try hello world"));
