2e6bea91b49d398cdac44d05a13a68933374128b
KSH701 <rkrk7636@naver.com>
2026-01-19 18:45:16 +0900

    01/19 문제풀이

---
+++ level1/035-시저-암호.js
@@ -0,0 +1,14 @@
+function solution(s, n) {
+  const arr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i].charCodeAt() === 32) {
+      arr.push(s[i].charCodeAt());
+    } else if (s[i] >="A"  && s[i] <= "Z") {
+      arr.push(((s[i].charCodeAt() - 65 + n) % 26) + 65);
+    } else if (s[i] >= "a" && s[i] <= "z") {
+      arr.push(((s[i].charCodeAt() - 97 + n) % 26) + 97);
+    }
+  }
+  return String.fromCharCode(...arr);
+}
+console.log(solution("a B z", 4));
