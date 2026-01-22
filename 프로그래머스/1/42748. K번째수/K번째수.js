f91348449f5630e48dda5decb869eba6201e239e
김수환 <125341405+SH701@users.noreply.github.com>
2026-01-22 09:26:47 +0900

    [level 1] Title: K번째수, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+  const result = [];
+  for (let i = 0; i < commands.length; i++) {
+    const [start, end, k] = commands[i];
+    const sliced = array.slice(start - 1, end);
+    sliced.sort((a, b) => a - b);
+    result.push(sliced[k - 1]);
+  }
+  return result;
+}
