d80caa40a541e8de80e36a735b2f88f9a6b15990
김수환 <125341405+SH701@users.noreply.github.com>
2026-05-13 15:08:02 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.16 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,15 +1,15 @@
 function solution(s) {
-  const arr = [];
-  let idx = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === " ") {
-      arr.push(" ");
-      idx = 0;
-    } else {
-      arr.push(idx % 2 === 0 ? s[i].toUpperCase() : s[i].toLowerCase());
-      idx++;
-    }
+  const a = []
+  let b = 0
+  for(let i=0;i<s.length;i++){
+      if(s[i]===' '){
+          a.push(" ")
+          b = 0
+      }else{
+          b%2===0? a.push(s[i].toUpperCase()) : a.push(s[i].toLowerCase())
+           b++
+      }
+     
   }
-  return arr.join("");
+    return a.join("")
 }
-
